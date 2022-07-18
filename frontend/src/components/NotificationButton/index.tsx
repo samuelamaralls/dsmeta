c7254706f240f363771d5064ee51c6c2a554a2c7
Samuel Rodrigues <rodrigues.samuel1221@gmail.com>
2022-07-18 09:05:32 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,27 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function App() {
+type Props = {
+
+  saleId: number;
+
+}
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+      console.log("Sucess");
+    })
+}
+
+function NotificationButton({saleId} : Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   );
 }
 
-export default App;
+export default NotificationButton;
